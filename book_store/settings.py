141b037914d687e7669f235fce63266f87b51d38
amir <amir.moloki8558@gmail.com>
2025-01-26 16:58:50 +0330

    add apps name in installed apps

--- book_store/settings.py
+++ book_store/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'authors',
+    'books',
+    'customers',
+    'discounts',
+    'genres',
+    'Language',
+    'publishers'
 ]
 
 MIDDLEWARE = [
