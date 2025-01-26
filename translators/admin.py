db919fdcc1372d4eed1c99d77a696d31b6a11859
amir <amir.moloki8558@gmail.com>
2025-01-26 17:35:08 +0330

    Register Translator model in admin with custom configuration

--- translators/admin.py
+++ translators/admin.py
@@ -1,3 +1,12 @@
+# admin.py
 from django.contrib import admin
+from .models import Translator
 
-# Register your models here.
+class TranslatorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'birth_date', 'nationality', 'rating')  # فیلدهای نمایش داده شده در لیست
+    search_fields = ('first_name', 'last_name', 'nationality')  # فیلدهایی که برای جستجو در نظر گرفته شده‌اند
+    list_filter = ('rating', 'nationality')  # فیلدهایی که می‌توانند برای فیلتر استفاده شوند
+    ordering = ('-rating',)  # مرتب‌سازی به ترتیب نزولی بر اساس نمره
+    list_per_page = 10  # تعداد نمایش هر صفحه از لیست
+
+admin.site.register(Translator, TranslatorAdmin)
