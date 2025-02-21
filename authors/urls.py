6cba1234a63dd8cf509fb670dd5f7bfea41402b5
Amir <amir.moloki8558@gmail.com>
2025-02-21 11:00:00 +0330

    authors urls

--- authors/urls.py
+++ authors/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from .views import AuthorListView, AuthorRetrieveView, AuthorFilterView, AuthorSearchView, AuthorCreateView, AuthorUpdateView, AuthorDeleteView
 
 urlpatterns = [
-    path('authors/', AuthorListView.as_view(), name='author-list'),
-    path('authors/<int:pk>/', AuthorRetrieveView.as_view(), name='author-retrieve'),
-    path('authors/filter/', AuthorFilterView.as_view(), name='author-filter'),
-    path('authors/search/', AuthorSearchView.as_view(), name='author-search'),
-    path('authors/create/', AuthorCreateView.as_view(), name='author-create'),
-    path('authors/update/<int:pk>/', AuthorUpdateView.as_view(), name='author-update'),
-    path('authors/delete/<int:pk>/', AuthorDeleteView.as_view(), name='author-delete'),
+    path('', AuthorListView.as_view(), name='author-list'),
+    path('<int:pk>/', AuthorRetrieveView.as_view(), name='author-retrieve'),
+    path('filter/', AuthorFilterView.as_view(), name='author-filter'),
+    path('search/', AuthorSearchView.as_view(), name='author-search'),
+    path('create/', AuthorCreateView.as_view(), name='author-create'),
+    path('update/<int:pk>/', AuthorUpdateView.as_view(), name='author-update'),
+    path('delete/<int:pk>/', AuthorDeleteView.as_view(), name='author-delete'),
 ]
