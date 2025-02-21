01d9f88261e39c9c51abc5b78c64681e60de85ee
Amir <amir.moloki8558@gmail.com>
2025-02-21 10:55:33 +0330

    update geners api urls

--- genres/urls.py
+++ genres/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import GenreListCreateView, GenreRetrieveUpdateDestroyView, GenreSearchView
 
 urlpatterns = [
-    path('genres/', GenreListCreateView.as_view(), name='genre-list-create'),
-    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-retrieve-update-destroy'),
-    path('genres/search/', GenreSearchView.as_view(), name='genre-search'),
+    path('', GenreListCreateView.as_view(), name='genre-list-create'),
+    path('<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-retrieve-update-destroy'),
+    path('search/', GenreSearchView.as_view(), name='genre-search'),
 ]
