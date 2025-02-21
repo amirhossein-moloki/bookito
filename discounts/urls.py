72f68aa50c0826228c26b70c501c3c4f5d2f0e35
Amir <amir.moloki8558@gmail.com>
2025-02-21 10:57:31 +0330

    update disconts urls

--- discounts/urls.py
+++ discounts/urls.py
@@ -8,9 +8,9 @@ from .views import (
 )
 
 urlpatterns = [
-    path('discount/create/', DiscountCreateView.as_view(), name='discount-create'),  # ایجاد تخفیف
-    path('discount/update/<int:pk>/', DiscountUpdateView.as_view(), name='discount-update'),  # ویرایش تخفیف
-    path('discount/delete/<int:pk>/', DiscountDeleteView.as_view(), name='discount-delete'),  # حذف تخفیف
-    path('discount/list/', DiscountListView.as_view(), name='discount-list'),  # مشاهده لیست تخفیف‌ها
-    path('discount/validate/', DiscountValidateView.as_view(), name='discount-validate'),  # بررسی اعتبار کد تخفیف
+    path('create/', DiscountCreateView.as_view(), name='discount-create'),  # ایجاد تخفیف
+    path('update/<int:pk>/', DiscountUpdateView.as_view(), name='discount-update'),  # ویرایش تخفیف
+    path('delete/<int:pk>/', DiscountDeleteView.as_view(), name='discount-delete'),  # حذف تخفیف
+    path('list/', DiscountListView.as_view(), name='discount-list'),  # مشاهده لیست تخفیف‌ها
+    path('validate/', DiscountValidateView.as_view(), name='discount-validate'),  # بررسی اعتبار کد تخفیف
 ]
